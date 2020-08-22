384fd3ee85ec090542d916d33a0ec9315e723c25
Arsalan Ghassemi <contact@arsadevs.com>
2020-08-22 17:21:32 +0200

    Added the View Email feature

--- mail/static/mail/js/inbox.js
+++ mail/static/mail/js/inbox.js
@@ -5,7 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  document.querySelector('#compose-form').addEventListener('submit', submit_email(e));
+  document.querySelector('#compose-form').addEventListener('submit', function() {
+    submit_email(event);
+    load_mailbox("sent");
+  });
+  document.querySelectorAll('#inbox-body').forEach(item => {
+    item.addEventListener('click', () => load_email(event));
+  })
 });
 
 function compose_email() {
@@ -21,7 +27,6 @@ function compose_email() {
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -39,6 +44,7 @@ function load_mailbox(mailbox) {
       for (var i = 0; i < emails.length; i++)
       {
         var newTr = document.createElement("tr");
+        newTr.dataset.emailid = emails[i].id;
         var newTd1 = document.createElement("td");
         var newTd2 = document.createElement("td");
         var newTd3 = document.createElement("td");
@@ -49,10 +55,10 @@ function load_mailbox(mailbox) {
         newTr.appendChild(newTd2);
         newTr.appendChild(newTd3);
         if (emails[i].read && mailbox == "inbox") {
-          newTr.className = "bg-secondary font-weight-normal";
+          newTr.className = "maillink bg-secondary font-weight-normal hovereffect";
         }
         else if (!emails[i].read && mailbox == "inbox") {
-          newTr.className = "bg-light font-weight-bold";
+          newTr.className = "maillink bg-light font-weight-bold hovereffect";
         }
         inboxbody.appendChild(newTr);
       }
@@ -84,9 +90,50 @@ function submit_email(e) {
       // Print result
       console.log(result);
   });
-  load_mailbox("sent");
 }
 
 function clearcontent(elementID) {
   document.querySelector(`#${elementID}`).innerHTML = "";
+}
+
+function load_email(ev) {
+  document.querySelector(".modal").style.display = "block";
+  console.log(ev.target.parentElement.dataset.emailid);
+  fetch(`/emails/${ev.target.parentElement.dataset.emailid}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        read: true
+    })
+  })  
+  fetch(`/emails/${ev.target.parentElement.dataset.emailid}`)
+  .then(response => response.json())
+  .then(email => {
+    // Print email
+    console.log(email);
+    document.querySelector("#mailsender").innerHTML = `<strong>From</strong> : ${email.sender}`;
+    document.querySelector("#mailrecipient").innerHTML = `<strong>To</strong> :`;
+    var recips = document.querySelector("#mailrecipient");
+    for (var c = 0; c < email.recipients.length; c++) {
+      var newSpan = document.createElement("span");
+      newSpan.innerHTML = email.recipients[c];
+      recips.appendChild(newSpan);
+    }
+    document.querySelector("#mailsubject").innerHTML = `<strong>Subject</strong> : ${email.subject}`;
+    document.querySelector("#maildate").innerHTML = `<strong>Date</strong> : ${email.timestamp}`;
+    document.querySelector("#mailcontent").innerHTML = `<strong>Body</strong> : ${email.body}`;
+  });
+  document.querySelector("#closemail").addEventListener("click", () => {
+    document.querySelector(".modal").style.display = "none";
+    load_mailbox("inbox");
+  })
+  document.querySelector("#archivemail").addEventListener("click", () => {
+    fetch(`/emails/${ev.target.parentElement.dataset.emailid}`, {
+      method: 'PUT',
+      body: JSON.stringify({
+          archived: true
+      })
+    })
+    document.querySelector(".modal").style.display = "none";
+    load_mailbox("inbox");
+  })
 }
